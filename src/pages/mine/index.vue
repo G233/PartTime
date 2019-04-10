c5ce28983de5be0299af3227712744aca074ad79
yds1454865804 <1454865804@qq.com>
2019-04-10 21:55:57 +0800

    新增 我的 页面 以及添加默认头像

---
+++ src/pages/mine/index.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div class="user">
+            <img src="../../../static/images/touxiang.jpg">
+            <div>
+                <b class="name">姓名</b>
+                <p>微信号</p>
+            </div>
+        </div>
+        <p></p><p></p><p></p><p></p>
+        <p></p><p></p> <p></p><p></p><p></p><p></p><p></p><p></p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            userInfo:{}
+        }
+    },
+}
+</script>
+
+<style scoped>
+img{
+    width: 200rpx;
+    height: 200rpx;
+    border-radius: 50%;
+}
+.name{
+    border-bottom: 1rpx solid #dddee1;
+    font-size: 42rpx;
+    text-align: center;
+}
+p{
+    border-bottom: 1rpx solid #dddee1;
+    font-size: 27rpx;
+    height: 100rpx;
+    text-align: center;
+    padding-top: 5rpx;
+}
+.user{
+    padding-top: 150rpx;
+    display: flex;
+    justify-content: space-between
+}
+</style>
