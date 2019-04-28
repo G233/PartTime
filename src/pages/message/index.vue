883bb1542be81d71260cc5a951e34876e3fe3aed
lg <214546439@qq.com>
2019-04-28 15:13:28 +0800

    完成站内信页面

--- src/pages/message/index.vue
+++ src/pages/message/index.vue
@@ -1,9 +1,14 @@
-
-
 <template>
   <div>
-    <div v-for="(item, index) in msglist" :key="index">
-      <div>{{item.title}}</div>
+    <div class="padding-top-lg">
+      <div v-for="(item, index) in msglist" :key="index">
+        <div class="msgt text-sm text-grey padding-lg">{{item.date}}</div>
+        <div class="msgc">
+          <div class="solid-bottom padding text-bold text-df text-blue">{{item.title}}</div>
+          <div class="text-df padding">{{item.details}}</div>
+        </div>
+      </div>
+      <i-load-more :loading="loding"/>
     </div>
   </div>
 </template>
@@ -15,23 +20,55 @@ export default {
   components: {},
   data() {
     return {
-      msglist: ""
+      loding: false,
+      msglist: [],
+      page: 0
     };
   },
   computed: {
-    //全局登录状态
+    time() {}
   },
-
-  methods: {},
+  //监听上拉触底，加载新数据
+  onReachBottom() {
+    this.page += 1;
+    this.getmsg();
+  },
+  methods: {
+    async getmsg() {
+      this.loding = true;
+      let msglist = await this.$request.request("/getmsg", {
+        data: { page: this.page }
+      });
+      this.loding = false;
+      for (let x of msglist.data.data.msglist) {
+        x.date = x.date.split("T")[0];
+      }
+      //合并新来的
+      this.msglist.push.apply(this.msglist, msglist.data.data.msglist);
+    }
+  },
   async created() {},
+
   async onShow() {
-    let msglist = await this.$request.request("/getmsg", {
-      data: {}
-    });
-    this.msglist = msglist.data.data.msglist;
+    this.getmsg();
   }
 };
 </script>
 
-<style scoped>
+<style s >
+.msgc {
+  background-color: white;
+  border-radius: 10rpx;
+  width: 90%;
+  height: 250rpx;
+  margin: auto;
+  margin-bottom: 20rpx;
+  margin-top: 20rpx;
+}
+.msgt {
+  text-align: center;
+}
+page {
+  background-color: #f1f1f1;
+}
 </style>
