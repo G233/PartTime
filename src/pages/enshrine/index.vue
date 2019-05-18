74e4dc24300e31433dc94ca0b22394b8d8ef1a51
yds1454865804 <1454865804@qq.com>
2019-05-18 16:20:16 +0800

    合并

--- src/pages/enshrine/index.vue
+++ src/pages/enshrine/index.vue
@@ -1,20 +1,4 @@
 <template>
-<<<<<<< HEAD
-    <div>
-        <view class="tip">tip:长按可删除或取消删除 
-            <i-tag class="i-tags" name="标签一" color="blue"@click="refresh" style="float:right;padding-right:40rpx;">刷新</i-tag>
-        </view>
-        <i-cell v-if="list.length==0" title="你还没有收藏工作呀！"></i-cell>
-        <view class="list" v-for="item in list" :key="index">
-            <i-cell :id='[item.jobId._id,index]'
-                @longpress="longpress"
-                @click="gotodetail(item.jobId)" 
-                i-class="i-cell-padding" 
-                :title="item.jobId.name" 
-                :label="item.jobId.choselei+'------------'+item.jobId.salary+'元/'+item.jobId.chosetime">
-            </i-cell>
-        </view>
-=======
   <div>
     <view class="tip">
       tip:长按可删除或取消删除
@@ -27,7 +11,7 @@
       >刷新</i-tag>
     </view>
     <i-cell v-if="list.length==0" title="你还没有收藏工作呀！"></i-cell>
-    <view class="list" v-for="item in list" :key="index" :class="item.jobId.delete?'delete':''">
+    <view class="list" v-for="item in list" :key="index" >
       <i-cell
         :id="[item.jobId._id,index]"
         @longpress="longpress"
@@ -37,7 +21,6 @@
         :label="item.jobId.choselei+'------------'+item.jobId.salary+'元/'+item.jobId.chosetime"
       ></i-cell>
     </view>
->>>>>>> dev
 
     <view v-if="visible" class="chexiao">
       <view>{{title}}</view>
@@ -73,56 +56,6 @@ export default {
   //     this.deletejobs();
   //     console.log("隐藏了");
   // },
-  methods: {
-    longpress(e) {
-      //console.log("aaa",e.currentTarget.id);
-      this.jobid = e.currentTarget.id;
-      this.visible = true;
-      //console.log(this.jobid.substr(0,24),'\n',this.jobid.substr(24,10));
-      this.index = this.jobid.substr(25, 10);
-      this.jobid = this.jobid.substr(0, 24);
-      this.title = this.list[this.index].jobId.name;
-      var time = setInterval(() => {
-        this.number -= 1;
-        console.log(this.number);
-        if (this.number <= 0 && this.visible) {
-          this.visible = false;
-          this.deletejobs();
-          clearInterval(time);
-          this.number = 2;
-        } else if (!this.visible) {
-          clearInterval(time);
-          this.number = 2;
-        } else return;
-      }, 1000);
-    },
-    canceljob() {
-      this.visible = false;
-      console.log("撤销");
-    },
-    async deletejobs() {
-      let msg = await this.$request.postRequest("/deleteenshrine", {
-        data: { jobId: this.jobid }
-      });
-      if (msg.data.code == 200) {
-        this.list.splice(Number(this.index), 1);
-        console.log(this.list);
-        $Message({
-          content: "删除成功！",
-          type: "success"
-        });
-      } else {
-        $Message({
-          content: "删除失败，等等再试试吧！",
-          type: "error"
-        });
-      }
-    },
-<<<<<<< HEAD
-    // onHide(){
-    //     this.deletejobs();
-    //     console.log("隐藏了");
-    // },
     methods: {
         longpress(e){
             console.log("aaa",e.currentTarget.id);
@@ -179,20 +112,12 @@ export default {
             this.$store.default.commit("changedetail", e);
             this.$WX.navigateTo("../detail/main");
         },
-=======
-    async refresh() {
-      let list = await this.$request.postRequest("/getenshrine");
-      //console.log(list.data.data);
-      this.list = list.data.data;
-      console.log(this.list);
->>>>>>> dev
     },
     //跳转详情页
     gotodetail(e) {
       this.$store.default.commit("changedetail", e);
       this.$WX.navigateTo("../detail/main");
     }
-  }
 };
 </script>
 
